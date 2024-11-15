a2d6c31a3626e698234bde56dbb4ca951bebadf1
dvankevich <dvankevich@gmail.com>
2024-11-15 10:00:39 +0200

    add BackLink component

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,10 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { Link, Outlet } from "react-router-dom";
+import { BackLink } from "../../components/BackLink/BackLink";
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
+  const location = useLocation();
+  const backLinkHref = location.state ?? "/movies";
   return (
     <div>
+      <BackLink to={backLinkHref}>Go back</BackLink>
       <h2>MovieDetailsPage</h2>
       <div>Now showing Movie details with id - {movieId}</div>
       <ul>
